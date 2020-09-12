e513f4497b76a3bce147d587d3c11435b2b0b15b
satlawa <p.satlawa@gmx.net>
2020-09-12 20:01:17 +0200

    added OS and kernel information

---
+++ build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/philipp/Code/cpp/ucy_cpp_system_monitor/src/format.cpp" "/home/philipp/Code/cpp/ucy_cpp_system_monitor/build/CMakeFiles/monitor.dir/src/format.cpp.o"
+  "/home/philipp/Code/cpp/ucy_cpp_system_monitor/src/linux_parser.cpp" "/home/philipp/Code/cpp/ucy_cpp_system_monitor/build/CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "/home/philipp/Code/cpp/ucy_cpp_system_monitor/src/main.cpp" "/home/philipp/Code/cpp/ucy_cpp_system_monitor/build/CMakeFiles/monitor.dir/src/main.cpp.o"
+  "/home/philipp/Code/cpp/ucy_cpp_system_monitor/src/ncurses_display.cpp" "/home/philipp/Code/cpp/ucy_cpp_system_monitor/build/CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "/home/philipp/Code/cpp/ucy_cpp_system_monitor/src/process.cpp" "/home/philipp/Code/cpp/ucy_cpp_system_monitor/build/CMakeFiles/monitor.dir/src/process.cpp.o"
+  "/home/philipp/Code/cpp/ucy_cpp_system_monitor/src/processor.cpp" "/home/philipp/Code/cpp/ucy_cpp_system_monitor/build/CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "/home/philipp/Code/cpp/ucy_cpp_system_monitor/src/system.cpp" "/home/philipp/Code/cpp/ucy_cpp_system_monitor/build/CMakeFiles/monitor.dir/src/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
